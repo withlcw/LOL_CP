08e7e5108a2d08f3d9294877c6c0cd7ef2d445b3
Cheolwon Lee <withlcw@gmail.com>
2019-12-02 00:12:09 +0900

    init LOL_CP project

---
+++ src/main/java/com/lolcp/demo/LolCpApplication.java
@@ -0,0 +1,13 @@
+package com.lolcp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolCpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lolcp/demo/LolCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lolcp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
